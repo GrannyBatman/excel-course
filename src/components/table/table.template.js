e0ad8193ab7cdd4699a77d335e659c2768002681
GrannyBatman <grannybatman111@gmail.com>
2020-08-13 15:42:52 +0300

    Finish table resize

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,19 +3,26 @@ const CODES = {
 	Z: 90,
 }
 
-function toCell(content) {
-	return `<div class="cell" contenteditable>${content}</div>`
+function toCell(_, col) {
+	return `<div class="cell" contenteditable data-col="${col}"></div>`
 }
 
 function createRow(index, content) {
-	return `<div class="row">
-            <div class="row-info">${index ? index : ''}</div>
+	const resize = index ? '<div class="row-resize" data-resize="row"></div>' : ''
+	return `<div class="row" data-type="resizable">
+						<div class="row-info">
+							${index ? index : ''}
+							${resize}
+						</div>
             <div class="row-data">${content}</div>
           </div>`
 }
 
-function toColumn(col) {
-	return `<div class="column">${col}</div>`
+function toColumn(col, index) {
+	return `<div class="column" data-type="resizable" data-col="${index}">
+						${col}
+						<div class="col-resize" data-resize="col"></div>
+					</div>`
 }
 
 function toChar(_, index) {
@@ -31,7 +38,7 @@ export function createTable(rowsCount = 15) {
 	rows.push(createRow(null, cols))
 
 	for (let i = 0; i < rowsCount; i++) {
-		const cells = new Array(colsCount).fill('').map(toCell).join('')
+		const cells = new Array(colsCount).fill('').map(toChar).map(toCell).join('')
 
 		rows.push(createRow(i + 1, cells))
 	}
